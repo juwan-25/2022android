8d2ffd26fe09f4d34c73b998c18467a29d3e8c5b
juwan-25 <s2109@e-mirim.hs.kr>
2022-04-19 20:04:55 +0900

    add 4-2 switch 버튼, 이미지 자동 변경

--- app/src/main/java/kr/hs/mirim2109/example/mirim_project_0414/MainActivity.java
+++ app/src/main/java/kr/hs/mirim2109/example/mirim_project_0414/MainActivity.java
@@ -10,10 +10,11 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
     RadioGroup rg;
-    CheckBox checkStart;
+    Switch switchStart;
     ImageView imgv;
     LinearLayout linearSub;
     @Override
@@ -23,17 +24,17 @@ public class MainActivity extends AppCompatActivity {
 
         linearSub = findViewById(R.id.linear_sub);
         rg = findViewById(R.id.rg);
-        checkStart = findViewById(R.id.check_start);
+        rg.setOnCheckedChangeListener(rgListener);
+        switchStart = findViewById(R.id.swtich_start);
         imgv = findViewById(R.id.imgv);
-        Button btnDone = findViewById(R.id.btn_done);
-        btnDone.setOnClickListener(btnListener);
-        checkStart.setOnCheckedChangeListener(checkListener);
+        switchStart.setOnCheckedChangeListener(switchListener);
 
     }
-    View.OnClickListener btnListener = new View.OnClickListener() {
+
+    RadioGroup.OnCheckedChangeListener rgListener = new RadioGroup.OnCheckedChangeListener() {
         @Override
-        public void onClick(View view) {
-            switch (rg.getCheckedRadioButtonId()){
+        public void onCheckedChanged(RadioGroup radioGroup, int selected_id) {
+            switch (selected_id) {
                 case R.id.rb_dog:
                     imgv.setImageResource(R.drawable.dog);
                     break;
@@ -43,14 +44,16 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.rb_rabbit:
                     imgv.setImageResource(R.drawable.rabbit);
                     break;
+
             }
         }
+
     };
 
-    CompoundButton.OnCheckedChangeListener checkListener = new CompoundButton.OnCheckedChangeListener() {
+    CompoundButton.OnCheckedChangeListener switchListener = new CompoundButton.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-            if(checkStart.isChecked()){
+            if (switchStart.isChecked()) {
                 linearSub.setVisibility(View.VISIBLE);
             } else {
                 linearSub.setVisibility(View.INVISIBLE);
