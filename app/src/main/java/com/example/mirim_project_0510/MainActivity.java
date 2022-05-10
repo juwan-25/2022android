99c2057889729acdb6fdaaac4c7bdd0cf8e2cc14
juwan-25 <s2109@e-mirim.hs.kr>
2022-05-10 18:44:09 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mirim_project_0510/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mirim_project_0510;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
