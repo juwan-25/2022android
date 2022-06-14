e734a75af3f9f083d0255463503c4bed4ce89c1e
juwan-25 <s2109@e-mirim.hs.kr>
2022-06-14 20:05:45 +0900

    Interval 수정

--- app/src/main/java/com/example/mirim_project_0614_2/MainActivity.java
+++ app/src/main/java/com/example/mirim_project_0614_2/MainActivity.java
@@ -5,20 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
     ViewFlipper vFlip;
+    EditText editInterval;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         vFlip = findViewById(R.id.vflip);
-        Button btnPrev = findViewById(R.id.btn_prev);
-        Button btnNext = findViewById(R.id.btn_next);
+        Button btnPrev = findViewById(R.id.btn_start);
+        Button btnNext = findViewById(R.id.btn_stop);
+        Button btnSecond = findViewById(R.id.btn_second);
+        editInterval = findViewById(R.id.edit_interval);
+
+
 
         btnPrev.setOnClickListener(btnListener);
         btnNext.setOnClickListener(btnListener);
+        btnSecond.setOnClickListener(btnListener);
 
     }
 
@@ -26,12 +33,15 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             switch (view.getId()){
-                case R.id.btn_prev:
+                case R.id.btn_start:
                     vFlip.startFlipping();
-                    vFlip.setFlipInterval(1000);
                     break;
-                case R.id.btn_next:
+                case R.id.btn_stop:
                     vFlip.stopFlipping();
+                    break;
+                case R.id.btn_second:
+                    int second = Integer.parseInt(editInterval.getText().toString());
+                    vFlip.setFlipInterval(second*1000);
                     break;
             }
 
