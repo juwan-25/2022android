a5daf163c36d67f99ae97b8dc1afe10da3887fe8
juwan-25 <s2109@e-mirim.hs.kr>
2022-07-25 11:29:56 +0900

    image 추가

--- app/src/main/java/com/example/mirim_project_0725/MainActivity.java
+++ app/src/main/java/com/example/mirim_project_0725/MainActivity.java
@@ -9,15 +9,18 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
     ActionBar.Tab tab1, tab2, tab3;
+
     MyFragment[] myFrags = new MyFragment[3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,17 +86,24 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
             layout.setOrientation(LinearLayout.VERTICAL);
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
             layout.setLayoutParams(params);
+            layout.setGravity(Gravity.CENTER);
+            ImageView image = new ImageView(super.getActivity());
+            layout.addView(image);
             if(tabName.equals("런쥔")){
                 layout.setBackgroundColor(Color.YELLOW);
+                image.setImageResource(R.drawable.renjun_is_baby);
             }
             if(tabName.equals("제노")){
                 layout.setBackgroundColor(Color.BLUE);
+                image.setImageResource(R.drawable.jeno_is_puppy);
             }
             if(tabName.equals("해찬")){
                 layout.setBackgroundColor(Color.RED);
+                image.setImageResource(R.drawable.haechan_is_bear);
             }
 
             return layout;
         }
     }
+
 }
