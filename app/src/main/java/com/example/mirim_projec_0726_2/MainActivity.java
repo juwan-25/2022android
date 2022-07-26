83677c620e29b4900fbcc7d8397a8d80e003a04e
juwan-25 <s2109@e-mirim.hs.kr>
2022-07-26 15:46:55 +0900

    목록대화 상자 완성

--- app/src/main/java/com/example/mirim_projec_0726_2/MainActivity.java
+++ app/src/main/java/com/example/mirim_projec_0726_2/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.mirim_projec_0726_2;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -16,7 +18,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout linear;
-    Button btn2;
+    Button btn2, btn5, btn6;
+    String[] listArr = {"런쥔", "해찬", "천러"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +32,47 @@ public class MainActivity extends AppCompatActivity {
         registerForContextMenu(btn2);
         Button btn3 = findViewById(R.id.btn_toast);
         btn3.setOnClickListener(toastListener);
+        Button btn4 = findViewById(R.id.btn_dialog);
+        btn4.setOnClickListener(dialogListener);
+        btn5 = findViewById(R.id.btn_dialog_list);
+        btn5.setOnClickListener(listDialogListener);
+        btn6 = findViewById(R.id.btn_radio_list);
+        btn6.setOnClickListener(radioDialogListener);
     }
+
+    View.OnClickListener listDialogListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+            dlg.setIcon(R.drawable.dialogicon);
+            dlg.setTitle("고양이는?");
+            dlg.setItems(listArr, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    btn5.setText(listArr[i]);
+                }
+            });
+            dlg.setNegativeButton("닫기", null);
+            dlg.show();
+        }
+    };
+
+    View.OnClickListener radioDialogListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+            dlg.setIcon(R.drawable.dialogicon);
+            dlg.setTitle("고양이는?");
+            dlg.setSingleChoiceItems(listArr, 0, new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    btn5.setText(listArr[i]);
+                }
+            });
+            dlg.setNegativeButton("닫기", null);
+            dlg.show();
+        }
+    };
 
     View.OnClickListener toastListener = new View.OnClickListener() {
         @Override
@@ -37,6 +80,24 @@ public class MainActivity extends AppCompatActivity {
             Toast t = Toast.makeText(MainActivity.this, "토스트위치변경연습", Toast.LENGTH_SHORT);
             t.setGravity(Gravity.CENTER,0,0);
             t.show();
+        }
+    };
+
+    View.OnClickListener dialogListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+            dialog.setIcon(R.drawable.dialogicon);
+            dialog.setTitle("대화상자 연습");
+            dialog.setMessage("여기는 대화상자가 들어갑니다");
+            dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    linear.setBackgroundColor(Color.CYAN);
+                }
+            });
+            dialog.setNegativeButton("취소", null);
+            dialog.show();
         }
     };
 
