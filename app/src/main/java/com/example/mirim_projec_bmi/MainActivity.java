4764854d1ecd165240713669d7ea8dc1f855bebb
juwan-25 <s2109@e-mirim.hs.kr>
2022-07-27 15:49:13 +0900

    activity_main 틀 완성

---
+++ app/src/main/java/com/example/mirim_projec_bmi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mirim_projec_bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
