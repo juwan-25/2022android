98d33d5dc8a1ad593aa2e6c08bbf2521c45df16c
juwan-25 <s2109@e-mirim.hs.kr>
2022-08-01 14:14:23 +0900

    list item add 및 remove 완성

---
+++ app/src/main/java/com/example/mirim_project_0801_4/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.mirim_project_0801_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> items = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    EditText edit1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listv = findViewById(R.id.listv);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, items);
+        listv.setAdapter(adapter);
+        edit1 = findViewById(R.id.edit1);
+        Button btnAdd = findViewById(R.id.btn_add);
+        btnAdd.setOnClickListener(btnAddListener);
+        listv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                items.remove(i);
+                adapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+    }
+    View.OnClickListener btnAddListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            items.add(edit1.getText().toString());
+            adapter.notifyDataSetChanged();
+        }
+    };
+}
